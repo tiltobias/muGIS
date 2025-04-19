e6d90119449b6bfe1a8ee56a042b5be8fee0620e
Tobias Andresen <tiltobias@gmail.com>
2025-04-19 11:28:14 +0200

    interesct 3 layers

--- muGIS/src/components/tool/tools/IntersectTool.tsx
+++ muGIS/src/components/tool/tools/IntersectTool.tsx
@@ -12,6 +12,7 @@ const IntersectTool: FC = () => {
 
   const [selectedLayer1, setSelectedLayer1] = useState<LayerData | undefined>(undefined);
   const [selectedLayer2, setSelectedLayer2] = useState<LayerData | undefined>(undefined);
+  const [selectedLayer3, setSelectedLayer3] = useState<LayerData | undefined>(undefined);
   const [newLayerName, setNewLayerName] = useState<string>("");
 
   // Update the new layer name when the selected layers change
@@ -22,7 +23,7 @@ const IntersectTool: FC = () => {
   }, [selectedLayer1, selectedLayer2]);
 
   const onFormSubmit = () => {
-    if (!selectedLayer1 || !selectedLayer2) {
+    if (!selectedLayer1 || !selectedLayer2 || !selectedLayer3) {
       alert("Please select two layers");
       return false;
     };
@@ -32,12 +33,15 @@ const IntersectTool: FC = () => {
     };
     const layer1 = selectedLayer1.featureCollection as FeatureCollectionPolygon;
     const layer2 = selectedLayer2.featureCollection as FeatureCollectionPolygon;
+    const layer3 = selectedLayer3.featureCollection as FeatureCollectionPolygon;
     layer1.features.forEach((feature1) => {
       layer2.features.forEach((feature2) => {
-        const result = intersect({type:"FeatureCollection",features:[feature1, feature2]});
-        if (result) {
-          outLayer.features.push(result);
-        }
+        layer3.features.forEach((feature3) => {
+          const result = intersect({type:"FeatureCollection",features:[feature1, feature2, feature3]});
+          if (result) {
+            outLayer.features.push(result);
+          }
+        });
       });
     });
     if (!outLayer || outLayer.features.length === 0) {
@@ -67,6 +71,14 @@ const IntersectTool: FC = () => {
         setSelectedLayer={setSelectedLayer2} 
         renderingType="fill"
         unselectableLayerIds={[selectedLayer1?.id]}
+      />
+
+      selected layer3: {selectedLayer3?.name}
+      <SelectLayer
+        selectedLayer={selectedLayer3} 
+        setSelectedLayer={setSelectedLayer3} 
+        renderingType="fill"
+        unselectableLayerIds={[selectedLayer1?.id, selectedLayer2?.id]}
       />
 
       <input type="text" value={newLayerName} onChange={(e)=>setNewLayerName(e.target.value)} />
