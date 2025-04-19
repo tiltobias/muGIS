fdc721ab907f6d046cd44ff90da687dbdea48135
Tobias Andresen <tiltobias@gmail.com>
2025-04-19 22:01:28 +0200

    ClipTool works for points and polygons

--- muGIS/src/components/tool/tools/ClipTool.tsx
+++ muGIS/src/components/tool/tools/ClipTool.tsx
@@ -1,9 +1,10 @@
 import { FC, useState, useEffect } from 'react';
 import useLayerStore, { LayerData, FeatureCollectionPolygon } from '../../../hooks/useLayerStore';
-import { difference } from '@turf/difference';
+import { intersect } from '@turf/intersect';
 import ToolModal from '../ToolModal';
 import SelectLayer from '../SelectLayer';
-import { combine } from '@turf/combine';
+import { pointsWithinPolygon } from '@turf/points-within-polygon';
+import { FeatureCollection, Point, MultiPoint } from 'geojson';
 
 const ClipTool: FC = () => {
 
@@ -11,35 +12,49 @@ const ClipTool: FC = () => {
     addLayer,
   } = useLayerStore();
 
-  const [selectedLayer1, setSelectedLayer1] = useState<LayerData | undefined>(undefined);
-  const [selectedLayer2, setSelectedLayer2] = useState<LayerData | undefined>(undefined);
+  const [selectedMaskLayer, setSelectedMaskLayer] = useState<LayerData | undefined>(undefined);
+  const [selectedLayer, setSelectedLayer] = useState<LayerData | undefined>(undefined);
   const [newLayerName, setNewLayerName] = useState<string>("");
 
   // Update the new layer name when the selected layers change
   useEffect(() => {
-    if (selectedLayer1 && selectedLayer2) {
-      setNewLayerName(`clip: ${selectedLayer1.name} - ${selectedLayer2.name}`);
+    if (selectedMaskLayer && selectedLayer) {
+      setNewLayerName(`clip(${selectedLayer.name}, ${selectedMaskLayer.name})`);
     }
-  }, [selectedLayer1, selectedLayer2]);
+  }, [selectedLayer, selectedMaskLayer]);
 
   const onFormSubmit = () => {
-    if (!selectedLayer1 || !selectedLayer2) {
+    if (!selectedMaskLayer || !selectedLayer) {
       alert("Please select two layers");
       return false;
     };
-    const outLayer: FeatureCollectionPolygon = {
+    let outLayer = {
       type: "FeatureCollection",
       features: [],
-    };
-    const layerBase = selectedLayer1.featureCollection as FeatureCollectionPolygon;
-    const layerSubtract = selectedLayer2.featureCollection as FeatureCollectionPolygon;
-    const subtractPolygon = (combine(layerSubtract) as FeatureCollectionPolygon).features[0];
-    layerBase.features.forEach((feature) => {
-      const result = difference({type:"FeatureCollection",features:[feature, subtractPolygon]});
-      if (result) {
-          outLayer.features.push(result);
-      }
-    });
+    } as FeatureCollection;
+    
+    if (selectedLayer.renderingType === "circle") {
+      outLayer = pointsWithinPolygon(
+        selectedLayer.featureCollection as FeatureCollection<Point|MultiPoint>, 
+        selectedMaskLayer.featureCollection as FeatureCollectionPolygon);
+    } else if (selectedLayer.renderingType === "fill") {
+      const maskLayer = selectedMaskLayer.featureCollection as FeatureCollectionPolygon;
+      const layer = selectedLayer.featureCollection as FeatureCollectionPolygon;
+      maskLayer.features.forEach((maskFeature) => {
+        layer.features.forEach((feature) => {
+          const result = intersect({type:"FeatureCollection",features:[feature, maskFeature]});
+          if (result) {
+            outLayer.features.push(result);
+          }
+        });
+      });
+    } else if (selectedLayer.renderingType === "line") {
+      throw new Error("Line clipping is not supported yet");
+    } else {
+      throw new Error("Unknown rendering type");
+    }
+
+
     if (!outLayer || outLayer.features.length === 0) {
       alert("No results found");
       return false;
@@ -54,19 +69,18 @@ const ClipTool: FC = () => {
   return (
     <ToolModal buttonLabel="Clip" onFormSubmit={onFormSubmit}>
       
-      selected base layer: {selectedLayer1?.name}
+      selected layer: {selectedLayer?.name}
       <SelectLayer 
-        selectedLayer={selectedLayer1} 
-        setSelectedLayer={setSelectedLayer1} 
-        renderingType="fill"
+        selectedLayer={selectedLayer} 
+        setSelectedLayer={setSelectedLayer} 
       />
 
-      selected layer to subtract: {selectedLayer2?.name}
+      selected mask layer: {selectedMaskLayer?.name}
       <SelectLayer
-        selectedLayer={selectedLayer2} 
-        setSelectedLayer={setSelectedLayer2} 
+        selectedLayer={selectedMaskLayer} 
+        setSelectedLayer={setSelectedMaskLayer} 
         renderingType="fill"
-        unselectableLayerIds={[selectedLayer1?.id]}
+        unselectableLayerIds={[selectedLayer?.id]}
       />
 
       <input type="text" value={newLayerName} onChange={(e)=>setNewLayerName(e.target.value)} />
