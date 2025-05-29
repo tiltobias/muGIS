689252bed8a0f7ff55244fb5583c4ba8ade03a4e
Tobias Andresen <tiltobias@gmail.com>
2025-05-30 00:58:01 +0200

    lucide icons for tools (instead of QGIS)

--- muGIS/src/components/icons/index.ts
+++ muGIS/src/components/icons/index.ts
@@ -1,11 +1,24 @@
 export { default as LayerLineIcon } from './LayerLine';
 export { default as LayerPointIcon } from './LayerPoint';
 export { default as LayerPolygonIcon } from './LayerPolygon';
-export { default as BufferIcon } from './Buffer';
-export { default as UnionIcon } from './Union';
-export { default as IntersectIcon } from './Intersect';
-export { default as DifferenceIcon } from './Difference';
-export { default as DissolveIcon } from './Dissolve';
-export { default as VoronoiIcon } from './Voronoi';
-export { default as BboxIcon } from './Bbox';
-export { default as ClipIcon } from './Clip';
+// export { default as BufferIcon } from './Buffer';
+// export { default as UnionIcon } from './Union';
+// export { default as IntersectIcon } from './Intersect';
+// export { default as DifferenceIcon } from './Difference';
+// export { default as DissolveIcon } from './Dissolve';
+// export { default as VoronoiIcon } from './Voronoi';
+// export { default as BboxIcon } from './Bbox';
+// export { default as ClipIcon } from './Clip';
+
+import { CircleDotDashed, SquaresIntersect, SquaresExclude, SquaresSubtract, SquaresUnite, Percent, Maximize, Scissors } from 'lucide-react';
+
+export {
+  CircleDotDashed as BufferIcon,
+  SquaresIntersect as IntersectIcon,
+  SquaresUnite as UnionIcon,
+  SquaresSubtract as DifferenceIcon,
+  SquaresExclude as DissolveIcon,
+  Percent as VoronoiIcon,
+  Scissors as ClipIcon,
+  Maximize as BboxIcon,
+}
