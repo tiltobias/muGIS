e8084023d63305b7a6c651446a0b53821f16db36
Tobias Andresen <tiltobias@gmail.com>
2025-05-30 01:51:23 +0200

    deleted unused QGIS icons (switched to lucide icons)

--- muGIS/src/components/icons/index.ts
+++ muGIS/src/components/icons/index.ts
@@ -1,14 +1,6 @@
 export { default as LayerLineIcon } from './LayerLine';
 export { default as LayerPointIcon } from './LayerPoint';
 export { default as LayerPolygonIcon } from './LayerPolygon';
-// export { default as BufferIcon } from './Buffer';
-// export { default as UnionIcon } from './Union';
-// export { default as IntersectIcon } from './Intersect';
-// export { default as DifferenceIcon } from './Difference';
-// export { default as DissolveIcon } from './Dissolve';
-// export { default as VoronoiIcon } from './Voronoi';
-// export { default as BboxIcon } from './Bbox';
-// export { default as ClipIcon } from './Clip';
 
 import { 
   CircleDotDashed, 
@@ -16,7 +8,6 @@ import {
   SquaresExclude, 
   SquaresSubtract, 
   SquaresUnite, 
-  // Percent, 
   Maximize, 
   Scissors, 
 } from 'lucide-react';
